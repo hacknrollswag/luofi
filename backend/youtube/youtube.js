a4c4342e8a096a914d0a4928ec67c77a39b01a5e
Luo Siyi <lluosiyi@gmail.com>
2022-01-07 17:58:42 +0800

    Create youtube player component

--- backend/youtube/youtube.js
+++ backend/youtube/youtube.js
@@ -1,11 +1,12 @@
-const YOUTUBE_SEARCH_API = 'https://www.googleapis.com/youtube/v3/search';
+import axios from "axios";
+const KEY = process.env.REACT_APP_API_KEY;
 
-export async function getServerSideProps() {
-    const res = await fetch(`${YOUTUBE_SEARCH_API}?part=snippet&id=5qap5aO4i9A&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`);
-    const data = await res.json();
-    return {
-        props: {
-            data
-        }
-    }
-}
+export const baseTerms = {
+  part: "snippet",
+  maxResults: 5,
+  key: KEY,
+};
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Player from './components/player.js';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Player/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
