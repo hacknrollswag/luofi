d1892ea7e82a93877ed6bff204f45f954ca43960
Luo Siyi <lluosiyi@gmail.com>
2022-01-07 10:18:23 +0800

    Add mock gifs

---
+++ frontend/assets/mockGifs.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        url:'https://media.giphy.com/media/VzlS9BIBAgvuH3EbRH/giphy.gif',
+    },
+    {
+        url:'https://media.giphy.com/media/gFPxNhzEWdFCCRAqf0/giphy.gif',
+    },
+    {
+        url:'https://media.giphy.com/media/1zgzISaYrnMAYRJJEr/giphy.gif',
+    },
+    {
+        url:'https://media.giphy.com/media/2UndXEgN5iBwc/giphy.gif',
+    },
+    {
+        url:'https://media.giphy.com/media/tVwYkNyossifS/giphy.gif',
+    },
+    {
+        url:'https://media.giphy.com/media/dHzHYeATMhsNhCq4k1/giphy.gif',
+    },
+    {
+        url: 'https://media.giphy.com/media/RMwgs5kZqkRyhF24KK/giphy.gif',
+    },
+];
