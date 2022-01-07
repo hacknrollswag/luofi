a62c1a86e9b4a78609837fff678778d2ba6ec39c
Xiong Jingya <66959387+xiongjya@users.noreply.github.com>
2022-01-07 22:38:29 +0800

    Edit search bar visual

--- frontend/src/component/search-bar.component.js
+++ frontend/src/component/search-bar.component.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
+const styles = {
+    width: 350,
+    height: 15,
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 30
+};
 
-export default class SearchBar extends Component {
-    render() {
-        return(
-            <form action="/" method="get">
-               <label htmlFor="song">
-                   <span className="no">Choose your song!</span>
-                </label> 
-                <input
-                    type="text"
-                    id="song"
-                    placeholder="Song Name, Artist"
-                    name="s"
-                />
-            </form>
-        );
-    };
+export default function SearchBar() {
+    const [searchVal, setSearchVal] = useState("");
+
+    return(
+        <form action="/" method="get">
+            <input
+                id="song"
+                type="text"
+                value={searchVal}
+                onChange={setSearchVal}
+                placeholder="Song name, Artist"
+                style={styles}
+            />
+        </form>
+    );
 }
