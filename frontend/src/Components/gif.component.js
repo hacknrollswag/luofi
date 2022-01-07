cdf521400085fa28ff2cc33bdfbcc730d1c28284
sheeepen <chengxi1020@gmail.com>
2022-01-07 19:35:51 +0800

    Add frontend modified:   frontend/package.json 	modified:   frontend/src/App.js 	new file:   frontend/src/Components/gif.component.js 	new file:   frontend/src/Components/search-bar.component.js 	new file:   frontend/src/Components/search-button.component.js 	modified:   frontend/src/index.js 	new file:   package-lock.json 	new file:   package.json

---
+++ frontend/src/Components/gif.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Gif extends Component {
+    render() {
+        return (
+            <img src="https://media.giphy.com/media/VzlS9BIBAgvuH3EbRH/giphy.gif" />   
+        )
+    }
+}
